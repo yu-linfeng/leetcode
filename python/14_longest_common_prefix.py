c3e4f619961efa7ec18b711fff03a0520e74c8b2
yu-linfeng <hellobug@outlook.com>
2018-01-09 17:23:46 +0800

    最长公共字符串前缀

--- python/14_longest_common_prefix.py
+++ python/14_longest_common_prefix.py
@@ -3,7 +3,7 @@ class Solution:
     给定一个字符列表找出最长的公共前缀。
     此处参考了leetcode中此题Discuss评论最高的解题方法，该方法的实现较为精妙，且优雅。
     使用了Python内置的zip函数，zip函数可以把列表合并，并创建一个元组对的列表；
-    enumerate函数可将一个可迭代的（iterable）/可遍历的对象（如列表、字符串）组成一个索引序列，多用于for循环计数
+    enumerate函数可将一个可迭代的（iterable）/可遍历的对象（如列表、字符串）组成一个索引序列，多用于for循环计数。
     """
     def longestCommonPrefix(self, strs):
         """
