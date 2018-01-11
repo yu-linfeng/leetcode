b7abc7a5987cc82e96efc3e47e76511ab36b4617
yu-linfeng <hellobug@outlook.com>
2018-01-11 19:41:20 +0800

    update

---
+++ python/06_zigzag_conversion.py
@@ -0,0 +1,16 @@
+class Solution:
+    """
+    """
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        
+
+if __name__ == "__main__":
+    solution = Solution()
+    s = "ABCDEF"
+    row = 3
+    print(solution.convert(s, row))
