7fc47d1dfa9f0737227b3f4b14b6b4c281c11927
yu-linfeng <hellobug@outlook.com>
2020-01-13 13:14:33 +0800

    实现indexOf。此题有KMP标准解法，可详细了解。但是此题较为简便的解法其思路仍然是"双指针"思想。

---
+++ leetcode-java/src/main/java/com/coderbuff/leetcode/_28/Test.java
@@ -0,0 +1,14 @@
+package com.coderbuff.leetcode._28;
+
+/**
+ * @author okevin
+ * @date 2020/1/13
+ */
+public class Test {
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "ll";
+        Solution solution = new Solution();
+        System.out.println(solution.strStr(haystack, needle));
+    }
+}
