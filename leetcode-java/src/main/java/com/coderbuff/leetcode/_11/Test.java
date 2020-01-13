d658a8f341b4fc1cd8ec32d9ca739aff95236318
yu-linfeng <hellobug@outlook.com>
2020-01-13 18:17:47 +0800

    求面积。遍历，还是遍历。一做算法题，就只想到暴力遍历。

---
+++ leetcode-java/src/main/java/com/coderbuff/leetcode/_11/Test.java
@@ -0,0 +1,13 @@
+package com.coderbuff.leetcode._11;
+
+/**
+ * @author okevin
+ * @date 2020/1/13
+ */
+public class Test {
+    public static void main(String[] args) {
+        int[] height = {1, 1};
+        Solution solution = new Solution();
+        System.out.println(solution.maxArea(height));
+    }
+}
