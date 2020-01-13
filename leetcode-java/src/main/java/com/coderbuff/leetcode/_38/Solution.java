b558faf1fae8d845a66038085497b81fb07516db
yu-linfeng <hellobug@outlook.com>
2020-01-13 14:59:36 +0800

    *外观数列，报数。记录个数+字符，一层一层计算。

---
+++ leetcode-java/src/main/java/com/coderbuff/leetcode/_38/Solution.java
@@ -0,0 +1,58 @@
+package com.coderbuff.leetcode._38;
+
+/**
+ * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。前五项如下：
+ *
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ * 1 被读作  "one 1"  ("一个一") , 即 11。
+ * 11 被读作 "two 1s" ("两个一"）, 即 21。
+ * 21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+ *
+ * 给定一个正整数 n（1 ≤ n ≤ 30），输出外观数列的第 n 项。
+ *
+ * 注意：整数序列中的每一项将表示为一个字符串。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: "1"
+ * 示例 2:
+ *
+ * 输入: 4
+ * 输出: "1211"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-and-say
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author okevin
+ * @date 2020/1/13
+ */
+public class Solution {
+    public String countAndSay(int n) {
+        String str = "1";
+        for (int i = 2; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+            char pre = str.charAt(0);
+            int count = 1;
+            for (int j = 1; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if (c == pre) {
+                    count++;
+                } else {
+                    sb.append(count).append(pre);
+                    pre = c;
+                    count = 1;
+                }
+            }
+            sb.append(count).append(pre);
+            str = sb.toString();
+        }
+        return str;
+    }
+}
---
+++ leetcode-java/src/main/java/com/coderbuff/leetcode/_38/Test.java
@@ -0,0 +1,12 @@
+package com.coderbuff.leetcode._38;
+
+/**
+ * @author okevin
+ * @date 2020/1/13
+ */
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countAndSay(4));
+    }
+}
