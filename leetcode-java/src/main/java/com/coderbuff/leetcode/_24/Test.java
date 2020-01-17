d3c65f061d05d2d2d546d4b638b8a6ca63b6a5ff
yu-linfeng <hellobug@outlook.com>
2020-01-17 15:07:34 +0800

    *两两交换链表中的节点，神奇的递归实现

---
+++ leetcode-java/src/main/java/com/coderbuff/leetcode/_24/Test.java
@@ -0,0 +1,11 @@
+package com.coderbuff.leetcode._24;
+
+/**
+ * @author okevin
+ * @date 2020/1/15
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1 % 2 == 0);
+    }
+}
